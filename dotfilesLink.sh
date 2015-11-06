8d44938dc984ab577cfaab17d6d7e10b6e4926de
naoki-k <k12naoki@gmail.com>
2015-11-06 10:05:59 +0900

    update tmux settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #! /bin/zsh
-ln -s ~/dotfiles/.vimrc ~/.vomrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
@@ -7,3 +7,4 @@ ln -s ~/dotfiles/.irbrc ~/.irbrc
 ln -s ~/dotfiles/.gemrc ~/.gemrc
 ln -s ~/dotfiles/.gitconfig.local/.gitconfig.local
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.config/powerline ~/.config/powerline
