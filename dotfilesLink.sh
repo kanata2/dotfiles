4b2df7d592d59decd49a72f5907b5ea44b15b36e
naoki-k <k12naoki@gmail.com>
2016-02-21 05:31:00 +0900

    Fix dotfilesLink

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,12 +1,8 @@
-#! /bin/zsh
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.irbrc ~/.irbrc
-ln -s ~/dotfiles/.gemrc ~/.gemrc
-ln -s ~/dotfiles/.gitconfig.local/.gitconfig.local
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.config/powerline ~/.config/powerline
-ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
+#! /bin/bash
+for dotfiles in $(echo .*[^.] |
+    tr ' ' '\n' |
+    awk '!/DS_|git.?$/')
+do
+    ln -fnsv $PWD/$dotfiles $HOME/$dotfiles
+done
 
