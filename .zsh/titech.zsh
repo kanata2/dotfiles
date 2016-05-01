94ec2d42a3b84202aa9e1f88294d51a957bcfbce
naoki-k <k12naoki@gmail.com>
2016-05-02 02:36:47 +0900

    Update titech.zsh to avoid repetition of [include]

--- .zsh/titech.zsh
+++ .zsh/titech.zsh
@@ -24,6 +24,7 @@ function unset_proxy() {
 
 if [ "`networksetup -getairportnetwork en0 | awk '{print $4}'`" = "$switch_trigger" ]; then
   echo "Switch to proxy for titech-pubnet"
+  sed -i '' '/\[include\]/d' $HOME/.gitconfig
   set_proxy
 else
   unset_proxy
